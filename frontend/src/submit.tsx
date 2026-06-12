88d2a72c94c43615e18aa27f3ecdf638e20f39ed
techwizard31 <istaprasad.patra@gmail.com>
2026-06-13 00:14:42 +0530

    fixing css issues

---
+++ frontend/src/submit.tsx
@@ -0,0 +1,18 @@
+interface SubmitButtonProps {
+  isLoading: boolean;
+  onClick: () => void;
+}
+
+export const SubmitButton = ({ isLoading, onClick }: SubmitButtonProps) => {
+  return (
+    <div className="submit-btn-container">
+      <button
+        onClick={onClick}
+        disabled={isLoading}
+        className="submit-btn"
+      >
+        {isLoading ? 'Analyzing Graph...' : 'Submit Pipeline Architecture'}
+      </button>
+    </div>
+  );
+};
